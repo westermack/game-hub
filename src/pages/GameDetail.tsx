9c51f079a09760bcd9c69b1af0057f8124b58c2f
Westermack Batanyita <westermack@gmail.com>
2023-07-14 19:57:59 +0300

    Build expandable text

--- src/pages/GameDetail.tsx
+++ src/pages/GameDetail.tsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
 import { Heading, Spinner, Text } from "@chakra-ui/react";
+import ExpandableText from "../components/ExpandableText";
 
 const GameDetail = () => {
   const { slug } = useParams();
@@ -13,7 +14,7 @@ const GameDetail = () => {
   return (
     <>
       <Heading>{game.name}</Heading>
-      <Text>{game.description_raw}</Text>
+      <ExpandableText>{game.description_raw}</ExpandableText>
     </>
   );
 };
