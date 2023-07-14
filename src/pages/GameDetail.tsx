c874c0618a1eecb14f5b6b86b8530ca19b0a9de9
Westermack Batanyita <westermack@gmail.com>
2023-07-14 22:19:33 +0300

    Build game trailer

--- src/pages/GameDetail.tsx
+++ src/pages/GameDetail.tsx
@@ -5,6 +5,7 @@ import ExpandableText from "../components/ExpandableText";
 import DefinitionItem from "../components/DefinitionItem";
 import CriticScore from "../components/CriticScore";
 import GameAttributes from "../components/GameAttributes";
+import GameTrailer from "../components/GameTrailer";
 
 const GameDetail = () => {
   const { slug } = useParams();
@@ -19,6 +20,7 @@ const GameDetail = () => {
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game} />
+      <GameTrailer gameId={game.id} />
     </>
   );
 };
