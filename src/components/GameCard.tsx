42e1328c4540ce93ad3d482c1d0c68700928afa0
Westermack Batanyita <westermack@gmail.com>
2023-07-14 19:05:11 +0300

    Refactor - move entity interfaces

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -7,7 +7,7 @@ import {
   Image,
   Text,
 } from "@chakra-ui/react";
-import { Game } from "../hooks/useGames";
+import { Game } from "../entities/Game";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,24 +2,11 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 import ms from "ms";
 import APIClient, { FetchResponse } from "../services/api-client";
 import useGameQueryStore from "../store";
-import { Genre } from "./useGenres";
-import { Platform } from "./usePlatforms";
+import { Game } from "../entities/Game";
 
 export interface Sort {
   label: string;
   value: string;
-}
-
-export interface Game {
-  id: number;
-  name: string;
-  slug: string;
-  background_image: string;
-  description_raw: string;
-  parent_platforms: { platform: Platform }[];
-  metacritic: number;
-  genres: Genre[];
-  rating_top: number;
 }
 
 const apiClient = new APIClient<Game>("/games");
